3239e0aaa087d680dd7cedba93f8f9651ee7c4e7
Santiago Hurtado <shurtado@ddoscom.tech>
2025-02-26 20:18:42 -0500

    Fix identation

--- main.tf
+++ main.tf
@@ -7,32 +7,32 @@ resource "aws_iam_user" "admin_user" {
 
 # Inline policy for the admin user that includes deployment permissions and role assumption
 resource "aws_iam_user_policy" "admin_policy" {
-  name   = "${var.admin_username}-policy"
-  user   = aws_iam_user.admin_user.name
+  name = "${var.admin_username}-policy"
+  user = aws_iam_user.admin_user.name
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Sid": "AllowDeployAccess",
-        "Effect": "Allow",
-        "Action": [
+        "Sid" : "AllowDeployAccess",
+        "Effect" : "Allow",
+        "Action" : [
           "s3:*",
           "lambda:*",
-          "cognito-idp:*",         
-          "cognito-sync:*",       
-          "cognito-identity:*",   
+          "cognito-idp:*",
+          "cognito-sync:*",
+          "cognito-identity:*",
           "cloudwatch:*",
           "logs:*",
           "iam:*",
           "ec2:*"
         ],
-        "Resource": "*"
+        "Resource" : "*"
       },
       {
-        "Sid": "AllowAssumeRole",
-        "Effect": "Allow",
-        "Action": "sts:AssumeRole",
-        "Resource": local.admin_assume_role_arns
+        "Sid" : "AllowAssumeRole",
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRole",
+        "Resource" : local.admin_assume_role_arns
       }
     ]
   })
@@ -52,15 +52,15 @@ resource "aws_iam_user" "atlantis_user" {
 
 # Inline policy for the Atlantis user that includes deployment permissions and role assumption
 resource "aws_iam_user_policy" "atlantis_policy" {
-  name   = "${var.atlantis_username}-policy"
-  user   = aws_iam_user.atlantis_user.name
+  name = "${var.atlantis_username}-policy"
+  user = aws_iam_user.atlantis_user.name
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Sid": "AllowDeployAccess",
-        "Effect": "Allow",
-        "Action": [
+        "Sid" : "AllowDeployAccess",
+        "Effect" : "Allow",
+        "Action" : [
           "s3:*",
           "lambda:*",
           "cognito-idp:*",
@@ -71,13 +71,13 @@ resource "aws_iam_user_policy" "atlantis_policy" {
           "iam:*",
           "ec2:*"
         ],
-        "Resource": "*"
+        "Resource" : "*"
       },
       {
-        "Sid": "AllowAssumeRole",
-        "Effect": "Allow",
-        "Action": "sts:AssumeRole",
-        "Resource": local.atlantis_assume_role_arns
+        "Sid" : "AllowAssumeRole",
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRole",
+        "Resource" : local.atlantis_assume_role_arns
       }
     ]
   })
--- variables.tf
+++ variables.tf
@@ -1,30 +1,30 @@
 variable "admin_username" {
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
   description = "The username for the administrator user in the master account."
 }
 
 variable "atlantis_username" {
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
   description = "The username for the Atlantis user in the master account."
 }
 
 variable "target_account_ids" {
-  type    = list(string)
-  default = []
+  type        = list(string)
+  default     = []
   description = "A list of target account IDs where roles will be assumed."
 }
 
 variable "admin_role_name" {
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
   description = "The name of the role in the target accounts that the administrator user can assume."
 }
 
 variable "atlantis_role_name" {
-  type    = string
-  default = ""
+  type        = string
+  default     = ""
   description = "The name of the role in the target accounts that the Atlantis user can assume."
 }
 
